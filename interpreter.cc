8ffc51c795476dcf0e224c6c615ac91ed3693fa5
Rupanshu Soi <rupanshusoi@gmail.com>
2023-05-20 02:24:55 -0700

    Remove dead code due to new design

--- interpreter.cc
+++ interpreter.cc
@@ -1,99 +1,5 @@
 #include "interpreter.hh"
 
-const vector<string> lex(const string& src)
-{
-  vector<string> tokens;
-  size_t idx = 0;
-  while (1) 
-  {
-    if (idx >= src.size()) break;
-    else if (src[idx] == ' ') idx++;
-    else if (src[idx] == '(' || src[idx] == ')')
-    {   
-      string tmp(1, src[idx]);
-      tokens.push_back(tmp);
-      idx++;
-    }   
-    else
-    {   
-      size_t tmp = idx;
-      while (src[tmp] != ')' && src[tmp] != ' ')
-        tmp++;
-      string s = src.substr(idx, tmp - idx);
-      tokens.push_back(s);
-      idx = tmp;
-    }   
-  }   
-  return tokens;
-}
-
-void test_lex(const vector<string>& tokens)
-{
-  for (auto& token: tokens)
-  {
-    cout << token << endl;
-  }
-}
-
-pair<int, vector<Node>> _parse(const vector<string>& tokens)
-{
-  vector<Node> ast;
-  size_t i = 0;
-  while (i < tokens.size() - 1)
-  {
-    i++;
-    if (tokens[i] ==  "(")
-    {
-      auto _pair = _parse(vector<string>(tokens.begin() + i, tokens.end()));
-      i += _pair.first;
-      Node node(_pair.second);
-      ast.push_back(node);
-    }
-    else if (tokens[i] == ")")
-    {
-      return make_pair(i, ast);
-    }
-    else
-    {
-      Node node(tokens[i]);
-      ast.push_back(node);
-    }
-  } 
-  assert(false);
-}
-
-vector<vector<Node>> parse(const vector<string>& tokens)
-{
-  vector<vector<Node>> parsed_lines;
-  size_t start = 0;
-  while (1)
-  {
-    auto parsed = _parse(vector<string>(tokens.begin() + start, tokens.end()));
-    parsed_lines.push_back(parsed.second);
-    if (start + parsed.first + 1 < tokens.size())
-    {
-      start += parsed.first + 1;
-    }
-    else break;
-  }
-  return parsed_lines;
-}
-
-void _test_parse(const vector<Node> ast, int indent = 0)
-{
-  for (auto &s: ast)
-  {
-    if (s.is_list) _test_parse(s.list, indent + 2);
-    else cout << string(indent, ' ') << s.arg << endl;
-  }
-}
-
-void test_parse(const vector<vector<Node>> parsed_lines)
-{
-  for (auto &line: parsed_lines)
-    _test_parse(line);
-}
-
 externref eval()
 {
   grow_rw_table_1(5, get_ro_table_0(0));
@@ -153,40 +59,21 @@ externref _fixpoint_apply(externref encode)
   attach_blob_ro_mem_0(get_ro_table_0(2));
   Op op = static_cast<Op>(get_i32_ro_mem_0(0));
 
-  switch (op)
+  if (op == EVAL)
   {
-    case BEGIN:
+    attach_blob_ro_mem_0(get_ro_table_0(3));
+    int is_list = get_i32_ro_mem_0(0);
+    if (!is_list)
     {
-      assert(false);
-      // const string src(buf, size);
-      // const auto tokens = lex(src);
-
-      // // Todo
-      // const auto line = parse(tokens)[0];
-
-      // Node wrapper(line);
-      // return eval();
+      attach_blob_ro_mem_0(get_ro_table_0(4));
+      size_t size = byte_size_ro_mem_0();
+      char *buf = (char *)malloc(size);
+      ro_mem_0_to_program_memory((int32_t)buf, 0, size);
+      string s(buf, size);
+      return create_blob_i32(stoi(s));
     }
-  
-    case EVAL:
-    {
-      attach_blob_ro_mem_0(get_ro_table_0(3));
-      int is_list = get_i32_ro_mem_0(0);
-      if (!is_list)
-      {
-        attach_blob_ro_mem_0(get_ro_table_0(4));
-        size_t size = byte_size_ro_mem_0();
-        char *buf = (char *)malloc(size);
-        ro_mem_0_to_program_memory((int32_t)buf, 0, size);
-        string s(buf, size);
-        return create_blob_i32(stoi(s));
-      }
-      return eval();      
-    }
-
-    default:
-    {
-      return apply(op);
-    }
+    return eval();      
   }
+  
+  return apply(op);
 }
--- interpreter.hh
+++ interpreter.hh
@@ -12,41 +12,12 @@ extern "C" {
 extern void ro_mem_0_to_program_memory( int32_t program_offset, int32_t ro_offset, int32_t len )
 __attribute( ( import_module( "wasi_snapshot_preview1" ), import_name( "ro_mem_0_to_program_memory" ) ) );
 
-extern void ro_mem_1_to_program_memory( int32_t program_offset, int32_t ro_offset, int32_t len )
-__attribute( ( import_module( "wasi_snapshot_preview1" ), import_name( "ro_mem_1_to_program_memory" ) ) );
-
 extern void program_memory_to_rw_0( int32_t rw_offset, int32_t program_offset, int32_t len )
 __attribute( ( import_module( "wasi_snapshot_preview1" ), import_name( "program_memory_to_rw_0" ) ) );
-
-extern void program_memory_to_rw_1( int32_t rw_offset, int32_t program_offset, int32_t len )
-__attribute( ( import_module( "wasi_snapshot_preview1" ), import_name( "program_memory_to_rw_1" ) ) );
 
 using namespace std;
 
 enum Op {BEGIN, EVAL, APPLY_ADD, APPLY_SUB, APPLY_MUL};
-
-struct Node
-{
-  bool is_list;
-  string arg;
-  vector<Node> list;
-
-  Node() = default;
-  Node(vector<Node> list) : is_list(true), arg(""), list(list) {};
-  Node (string arg) : is_list(false), arg(arg), list(vector<Node>()) {};
-
-  Node& operator[](size_t idx)
-  {
-    assert(is_list && arg == "");
-    return list[idx];
-  }
-
-  template<class Archive>
-  void serialize(Archive& archive)
-  {
-    archive(is_list, arg, list);
-  }
-};
 
 void fio(const char *txt, size_t size)
 {
