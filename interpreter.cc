a36bf8940ce972f14bde774b273c1ca3130f5f5c
Rupanshu Soi <rupanshusoi@gmail.com>
2023-06-05 21:58:48 -0700

    Add ugly code that does proper lookups

--- interpreter.cc
+++ interpreter.cc
@@ -3,7 +3,9 @@
 externref apply_lambda()
 {
   atbrom(0, getrotarg(0, 1));
-  int val = geti32rom(0);
+  int x = geti32rom(0);
+  atbrom(0, getrotarg(0, 2));
+  int y = geti32rom(0);
 
   attrot(2, getrotarg(0, 0));
   attrot(1, getrotarg(2, 2));
@@ -13,11 +15,41 @@ externref apply_lambda()
   set(1, 1, getrot(1, 1));
   set(1, 2, getrot(1, 2));
   set(1, 3, getrot(1, 3));
-  set(1, 4, i32(val));
+
+  // Lookup is a tree that has two sub-trees:
+  //   First contains the identifiers
+  //   Second contains the values
+
+  // First half:
+  grow(2, 2, getrot(0, 0));
+  string xstr = "x";
+  grow_rw_mem_0_pages(2);
+  program_memory_to_rw_0(0, (int32_t)xstr.c_str(), xstr.size());
+  set(2, 0, create_blob_rw_mem_0(xstr.size()));
+  string ystr = "y";
+  grow_rw_mem_0_pages(2);
+  program_memory_to_rw_0(0, (int32_t)ystr.c_str(), ystr.size());
+  set(2, 1, create_blob_rw_mem_0(xstr.size()));
+
+  externref first = treerw(2, 2);
+
+  // Second half:
+  grow(2, 2, getrot(0, 0));
+  set(2, 0, i32(x)); 
+  set(2, 1, i32(y)); 
+  externref second = treerw(2, 2);
+
+  // Combine
+  grow(2, 2, getrot(0, 0));
+  set(2, 0, first);
+  set(2, 1, second);
+  
+  set(1, 4, treerw(2, 2));
 
   for (int i = 0; i < size - PRELUDE; i++)
     setarg(1, i, getrotarg(1, i));
 
+  // return treerw(1, size);
   return thunk(treerw(1, size));
 }
 
@@ -87,6 +119,31 @@ externref eval_list()
   return thunk(treerw(0, size));
 }
 
+externref lookup(const char* id)
+{
+  attrot(1, getrot(0, ENV));
+
+  // First iterate through the first subtree to find id
+  attrot(2, getrot(1, 0));
+  int size = size_ro_table_2();
+  int idx = -1;
+  for (int i = 0; i < size; i++)
+  {
+    atbrom(0, getrot(2, i));
+    size_t size = byte_size_ro_mem_0();
+    char *buf = (char *)malloc(size + 1);
+    buf[size] = 0;
+    from_ro_mem_0((int32_t)buf, size);
+
+    if (!strcmp(id, buf)) { idx = i; break; }
+  }
+  fassert(idx != -1);
+
+  // Now get the corresponding entry from the second subtree
+  attrot(2, getrot(1, 1));
+  return getrot(2, idx);
+}
+
 externref eval_single()
 {
   atbrom(0, getrotarg(0, 0));
@@ -109,8 +166,7 @@ externref eval_single()
   }
   else if (isalpha(buf[0]))
   {
-    // Fixme
-    return getrot(0, ENV);
+    return lookup(buf);
   }
   return i32(strtol(buf, nullptr, 10));
 }
