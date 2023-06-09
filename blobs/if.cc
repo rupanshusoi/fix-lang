bb530e0947ee5fddf11e3600b368210da84f2642
Rupanshu Soi <rupanshusoi@gmail.com>
2023-06-09 16:34:58 -0700

    Update if blob to use macros

--- blobs/if.cc
+++ blobs/if.cc
@@ -7,12 +7,12 @@ extern "C" {
 __attribute__(( export_name("_fixpoint_apply")))
 externref _fixpoint_apply(externref encode)
 {
-  attach_tree_ro_table_0(encode);
-  attach_blob_ro_mem_0(get_ro_table_0(2));
-  int predicate = get_i32_ro_mem_0(0);
+  attrot(0, encode);
+  atbrom(0, getrot(0, 2));
+  int predicate = geti32rom(0);
   if (predicate)
   {
-    return get_ro_table_0(3); 
+    return getrot(0, 3); 
   }
-  return get_ro_table_0(4);
+  return getrot(0, 4);
 }
