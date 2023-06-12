8b82881c128f908f631b73670055487622b9c171
Rupanshu Soi <rupanshusoi@gmail.com>
2023-06-11 19:01:06 -0700

    Remove extraneous returns

--- util.hh
+++ util.hh
@@ -28,8 +28,8 @@ constexpr auto geti32rom(int midx) {
   return (fs[midx])(0);
 }
 
-constexpr auto geti32romz(int midx) {
-  return get_i32_ro_mem_0(0);
+constexpr auto geti32romz() {
+  return geti32rom(0);
 }
 
 constexpr auto getrotarg(int tidx, int idx) {
@@ -44,14 +44,14 @@ constexpr auto set(int tidx, int idx, externref ref) {
   constexpr std::array fs = {set_rw_table_0, set_rw_table_1, set_rw_table_2};
   assert(tidx >= 0);
   assert(tidx <= fs.size());
-  return (fs[tidx])(idx, ref);
+  (fs[tidx])(idx, ref);
 }
 
 constexpr auto setarg(int tidx, int idx, externref ref) {
   constexpr std::array fs = {set_rw_table_0, set_rw_table_1, set_rw_table_2};
   assert(tidx >= 0);
   assert(tidx <= fs.size());
-  return (fs[tidx])(idx + PRELUDE, ref);
+  (fs[tidx])(idx + PRELUDE, ref);
 }
 
 constexpr auto atbrom(int midx, externref ref) {
@@ -59,11 +59,11 @@ constexpr auto atbrom(int midx, externref ref) {
                              attach_blob_ro_mem_2, attach_blob_ro_mem_3};
   assert(midx >= 0);
   assert(midx <= fs.size());
-  return (fs[midx])(ref);
+  (fs[midx])(ref);
 }
 
 constexpr auto atbromz(externref ref) {
-  return attach_blob_ro_mem_0(ref);
+  attach_blob_ro_mem_0(ref);
 }
 
 constexpr auto attrot(int tidx, externref ref) {
@@ -71,14 +71,14 @@ constexpr auto attrot(int tidx, externref ref) {
                              attach_tree_ro_table_2, attach_tree_ro_table_3};
   assert(tidx >= 0);
   assert(tidx <= fs.size());
-  return (fs[tidx])(ref);
+  (fs[tidx])(ref);
 }
 
 constexpr auto grow(int tidx, int size, externref ref) {
   constexpr std::array fs = {grow_rw_table_0, grow_rw_table_1, grow_rw_table_2};
   assert(tidx >= 0);
   assert(tidx <= fs.size());
-  return (fs[tidx])(size, ref);
+  (fs[tidx])(size, ref);
 }
 
 constexpr auto treerw(int tidx, int size) {
